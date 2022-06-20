6477dab1caf942ecd08f5d6ae714d7ad63a005bc
Süleyman AYDIN <101866156+Slymnaydin61@users.noreply.github.com>
2022-06-20 22:30:34 +0300

    V 1.01.00

--- _Wordle_/Assets/Script/JokerButton.cs
+++ _Wordle_/Assets/Script/JokerButton.cs
@@ -6,24 +6,32 @@ using UnityEngine.UI;
 public class JokerButton : MonoBehaviour
 {
     WordManager wordManager;
+    ContentController contentController;
+    [SerializeField] List<RowController> rowController;
     [SerializeField] List<char> wordChar;
     [SerializeField] List<Button> keyboard;
-    [SerializeField] List<Image> myButtonImages;
-    char[] word;
+    [SerializeField] List<Button> wordsButton;
     [SerializeField] string cuurentWord;
+    [SerializeField] int jokerCount=3;
+    char[] word;
+    int listCount;
+    int index;
+    float showTimer;
+    bool isHintGiven;
+  
 
 
     void Awake()
     {
+        contentController = FindObjectOfType<ContentController>();
         wordManager=FindObjectOfType<WordManager>();
-
-
     }
     void Update()
     {
         FindCurrentWord();
         TurnWordToArrayChar();
-        Debug.Log(cuurentWord);
+        ButtonToList();
+       // DeleteJokerLetter();
     }
     void FindCurrentWord()
     {
@@ -41,18 +49,53 @@ public class JokerButton : MonoBehaviour
             }
         }
     }
+    void ButtonToList()
+    {
+        foreach (var button in keyboard)
+        {
+            for(int i=0; i<wordChar.Count; i++)
+            {
+                if (button.name == wordChar[i].ToString()&&listCount<5)
+                {
+                    wordsButton.Add(button);
+                    listCount++;
+                }
+            }
+        }
+    }
+    void DeleteJokerLetter()
+    {
+        showTimer -= Time.deltaTime;
+        if(showTimer<0&&isHintGiven)
+        {
+            rowController[contentController._index].blocks[index].UpdateText(' ');
+        }
+    }
 
     public void GiveJokerLetter()
     {
-       foreach( var item in wordManager.buttonChar)
-       {
-            for(int i = 0; i < wordChar.Count; i++)
+        if(jokerCount>0)
+        {
+            RandomizeHintLetter();
+            ColorKeyBoard();
+            showTimer = 2f;
+            isHintGiven = true;
+            jokerCount--;
+        }
+    }
+    void RandomizeHintLetter()
+    {
+        index = Random.Range(0, wordsButton.Count);
+        for(int i=0; i<wordChar.Count;i++)
+        {
+            if (wordsButton[index].name == wordChar[i].ToString())
             {
-                if(item==wordChar[i])
-                {
-                    //myButtonImages.Add()
-                }
+                rowController[contentController._index].blocks[i].UpdateText(wordChar[i]);
             }
-       }
+        }
+    }
+    void ColorKeyBoard()
+    {
+        wordsButton[index].GetComponentInChildren<Image>().color = wordManager.buttonColors[0];
     }
 }
